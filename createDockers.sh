f62c00722324cb24fbe294819d26b31c34a58bf3
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-12-04 19:27:58 +0200

    Using checksum

--- createDockers.sh
+++ createDockers.sh
@@ -6,4 +6,4 @@ cp mongoDB.Dockerfile Dockerfile
 docker build -t s2_mongodb:v1 .
 docker save s2_mongodb:v1 -o s2_docker/mongodb.tar
 ls -l s2_docker
-cache store $SEMAPHORE_PROJECT_NAME-$SEMAPHORE_GIT_SHA s2_docker
+cache store $(checksum go.Dockerfile)-$(checksum mongoDB.Dockerfile) s2_docker
